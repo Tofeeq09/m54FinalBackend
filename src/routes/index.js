dc45bd5475de3177f03f877f77395dbf6754a90f
Tofeeq <tofeeq09@gmail.com>
2024-03-13 16:07:10 +0000

    enhancement: new friend added for friend list

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@ const userRoutes = require("./userRoutes");
 const groupRoutes = require("./groupRoutes");
 const eventRoutes = require("./eventRoutes");
 const postRoutes = require("./postRoutes");
+const friendRoutes = require("./friendRoutes");
 
 module.exports = {
   userRoutes,
   groupRoutes,
   eventRoutes,
   postRoutes,
+  friendRoutes,
 };
